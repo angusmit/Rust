bbdd52a0c625dc29a871876ec054068d339ccec6
angusmit <angusnsh@gmail.com>
2024-07-22 22:53:21 +0100

    ch2 - project, guessing game

---
+++ Tutorial/guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\angus\github\Rust\Tutorial\guessing_game\target\debug\deps\guessing_game.exe: src/main.rs
+
+C:\Users\angus\github\Rust\Tutorial\guessing_game\target\debug\deps\guessing_game.d: src/main.rs
+
+src/main.rs:
---
+++ Tutorial/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\angus\github\Rust\Tutorial\guessing_game\target\debug\guessing_game.exe: C:\Users\angus\github\Rust\Tutorial\guessing_game\src\main.rs
